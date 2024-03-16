4b57c4bd47dc05944edaf2307f9cdf6f10d2c546
aleksandrasilina <aleksandrasilina@gmail.com>
2024-03-16 18:19:16 +0300

    add str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def get_upper(text):
+    """Возвращает строку заглавными буквами"""
     return text.upper()
+
+
+def get_title(text):
+    """Делает заглавными первые буквы каждого слова в строке"""
+    return text.title()
